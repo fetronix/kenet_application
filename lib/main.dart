5cada938dcf724f61756dccf1713a3b6932e87ad
hacks254 <fnzomo@kenet.or.ke>
2025-01-17 08:10:35 +0300

    edists

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kenet_application/allUrls.dart';
 import 'login_screen.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:io'; // Import for Platform class
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter/foundation.dart'; // Import for kIsWeb
 
 void main() {
   runApp(MyApp());
@@ -23,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: VersionCheckScreen(),
+      home: LoginScreen(), // Directly navigate to the LoginScreen
       routes: {
         '/login': (context) => LoginScreen(),
       },
@@ -31,143 +24,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class VersionCheckScreen extends StatefulWidget {
+class VersionCheckScreen extends StatelessWidget {
   const VersionCheckScreen({super.key});
-
-  @override
-  _VersionCheckScreenState createState() => _VersionCheckScreenState();
-}
-
-class _VersionCheckScreenState extends State<VersionCheckScreen> {
-  final String latestVersionUrl = ApiUrls.Appversion;
-  String latestVersion = "0.0.0";
-
-  @override
-  void initState() {
-    super.initState();
-    _showPlatformSnackbar();
-    _checkForUpdate();
-  }
-
-  // Display a snackbar with the platform information
-  void _showPlatformSnackbar() {
-    String platform = "";
-
-    if (kIsWeb) {
-      platform = "Web";
-    } else if (Platform.isAndroid) {
-      platform = "Android";
-    } else if (Platform.isIOS) {
-      platform = "iOS";
-    } else if (Platform.isLinux) {
-      platform = "Linux";
-    } else if (Platform.isWindows) {
-      platform = "Windows";
-    } else if (Platform.isMacOS) {
-      platform = "macOS";
-    } else if (Platform.isFuchsia) {
-      platform = "Fuchsia";
-    }
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Running on $platform")),
-      );
-    });
-  }
-
-  // Check if the app version is outdated
-  void _checkForUpdate() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    String currentVersion = packageInfo.version;
-
-    try {
-      final response = await http.get(Uri.parse(latestVersionUrl));
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
-        latestVersion = data['latest_version'];
-        String updateUrl = data['update_url'];
-
-        if (_isUpdateAvailable(currentVersion, latestVersion)) {
-          if (Platform.isAndroid) {
-            _launchPlayStoreUpdate(updateUrl); // For Android, open Play Store
-          } else if (Platform.isIOS) {
-            _launchAppStoreUpdate(updateUrl); // For iOS, open App Store
-          } else {
-            _showUpdateDialog(updateUrl); // For other platforms, show dialog
-          }
-        } else {
-          _navigateToLogin();
-        }
-      } else {
-        print("Failed to load the latest version from server");
-      }
-    } catch (e) {
-      print("Error fetching the latest version: $e");
-    }
-  }
-
-  bool _isUpdateAvailable(String currentVersion, String latestVersion) {
-    return currentVersion != latestVersion;
-  }
-
-  void _showUpdateDialog(String updateUrl) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Update Required"),
-        content: Text("A new version of the app is available. Please update to continue."),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              _launchUpdateURL(updateUrl);
-            },
-            child: Text("Update Now"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _launchUpdateURL(String updateUrl) async {
-    if (await canLaunch(updateUrl)) {
-      await launch(updateUrl);
-    } else {
-      throw 'Could not launch $updateUrl';
-    }
-  }
-
-  // Launch the Play Store for updating the app
-  void _launchPlayStoreUpdate(String updateUrl) async {
-    if (await canLaunch(updateUrl)) {
-      await launch(updateUrl);
-    } else {
-      throw 'Could not launch Play Store update URL';
-    }
-  }
-
-  // Launch the App Store for updating the app
-  void _launchAppStoreUpdate(String updateUrl) async {
-    if (await canLaunch(updateUrl)) {
-      await launch(updateUrl);
-    } else {
-      throw 'Could not launch KENET Store update URL';
-    }
-  }
-
-  void _navigateToLogin() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => LoginScreen()),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Checking for Updates...")),
+      appBar: AppBar(title: Text("Welcome to KENET Assets")),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Text(
+          'No version check in this build.',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
